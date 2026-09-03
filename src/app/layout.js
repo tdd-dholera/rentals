818dd408cbde3be3fbe75903c10e2db37dc2bfb6
Vikas Gupta <vikasnwrdr2001@gmail.com>
2026-09-03 11:25:17 +0530

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -44,7 +44,6 @@ export default function RootLayout({ children }) {
     height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
         />
-        
         {children}</body>
     </html>
   );
