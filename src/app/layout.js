407fdbae1443112e850c6f7dd5adce3da3c9b169
Vikas Gupta <vikasnwrdr2001@gmail.com>
2026-09-03 11:02:24 +0530

    chnages

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,29 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={geist.variable}>
-      <body>{children}</body>
+      <head>
+        <Script
+          id="gtm-head"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-WLBLBNTS');`,
+          }}
+        />
+      </head>
+      <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WLBLBNTS"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+        {children}</body>
     </html>
   );
 }
